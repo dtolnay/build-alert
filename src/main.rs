0370996a7a1a077ac2d22cb7e7622d29fa73c77e
David Tolnay <dtolnay@gmail.com>
2023-03-26 15:27:32 -0700

    Add a main.rs with test message

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+build_alert::yellow! {"
+NOTE:  it worked!
+  This example message only appears when building this crate
+  in its git repo, not when depending on it via crates.io.
+"}
+
+fn main() {}
